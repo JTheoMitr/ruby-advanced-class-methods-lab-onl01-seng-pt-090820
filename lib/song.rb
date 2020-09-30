3b14edb6af8d13b5b3e1dff17f7ceabf08ccb77f
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-30 05:28:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,5 +51,12 @@ class Song
     song.artist_name = name.split(" - ")[0]
     song
   end
+  
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = name.split(" - ")[1].chomp(".mp3")
+    song.artist_name = name.split(" - ")[0]
+    song.save
+  end
 
 end
